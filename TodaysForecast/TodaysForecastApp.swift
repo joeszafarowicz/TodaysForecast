15ff9e00638705eabd07e27c40a288588af26c69
Joseph Szafarowicz <joeszafarowicz@gmail.com>
2021-01-08 11:11:34 -0500

    Initial Commit

---
+++ TodaysForecast/TodaysForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodaysForecastApp.swift
+//  TodaysForecast
+//
+//  Created by Joseph Szafarowicz on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct TodaysForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
